3c0eb772459799be68f5293bc51a91ec4e978671
tdelpup <tomasgdelpup@gmail.com>
2025-01-20 00:25:09 -0300

    refactor: move exception to utilities folder

--- app/tests/utils/exceptions.py
+++
@@ -1,7 +0,0 @@
-from fastapi import HTTPException, status
-
-
-class NotUniqueException(HTTPException):
-    def __init__(self, item: str) -> None:
-        detail = f"{item.capitalize()} already exists."
-        super().__init__(status_code=status.HTTP_409_CONFLICT, detail=detail)
