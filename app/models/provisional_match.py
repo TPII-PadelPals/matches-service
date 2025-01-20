9cf8bab7e23a8d1e6f9a26ecb0091ed079d5b962
tdelpup <tomasgdelpup@gmail.com>
2025-01-20 00:35:26 -0300

    refactor: remove unused ProvisionalMatchCreate method

--- app/models/provisional_match.py
+++ app/models/provisional_match.py
@@ -17,15 +17,6 @@ class ProvisionalMatchBase(SQLModel):
 class ProvisionalMatchCreate(ProvisionalMatchBase):
     pass
 
-    def get_values(self):
-        return {
-            "player_id_1": self.player_id_1,
-            "player_id_2": self.player_id_2,
-            "court_id": self.court_id,
-            "time": self.time,
-            "date": self.date,
-        }
-
 
 # Database model, database table inferred from class name
 class ProvisionalMatch(ProvisionalMatchBase, table=True):
