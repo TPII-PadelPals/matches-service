9bd12f5581a56f3bcc691283facdb6fbb509fc7c
Belletti Gabriel Ignacio <44848915+GIBelletti@users.noreply.github.com>
2025-03-10 16:28:20 -0300

    test: add test varius players returns

--- app/tests/api/routes/test_players.py
+++ app/tests/api/routes/test_players.py
@@ -35,25 +35,6 @@ async def test_get_player_matches_returns_all_matches_associated_to_player(
         f"{test_settings.API_V1_STR}/players/{user_public_id}/matches/",
         headers=x_api_key_header,
     )
-    # {
-    #     "data": [
-    #         {
-    #             "public_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-    #             "court_id": 0,
-    #             "time": 0,
-    #             "date": "2025-03-10",
-    #             "status": "Provisional",
-    #             "match_players": [
-    #                 {
-    #                     "user_public_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-    #                     "match_public_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-    #                     "reserve": "Provisional",
-    #                 }
-    #             ]
-    #         }
-    #     ],
-    #     "count": 0
-    # }
     assert response.status_code == 200
     content = response.json()
     assert content["count"] == n_matches
@@ -124,5 +105,47 @@ async def test_get_player_matches_returns_all_matches_associated_to_player_multi
         assert len(response_data["match_players"]) == 1
         for player_data in response_data["match_players"]:
             assert player_data["user_public_id"] == user_public_id
+            assert player_data["match_public_id"] in match_public_ids
+            assert player_data["reserve"] == ReserveStatus.provisional
+
+
+async def test_get_player_matches_returns_all_matches_associated_to_multiple_player(
+    async_client: AsyncClient, x_api_key_header: dict[str, str]
+) -> None:
+    # Create match
+    n_matches = 4
+    match_public_ids = []
+    for i in range(n_matches):
+        _data = serialize_match_data(court_id=0, time=i, date="2024-11-25")
+        _response = await create_match(async_client, x_api_key_header, _data)
+        _content = _response.json()
+        match_public_ids.append(_content["public_id"])
+
+    # Add players to match
+    user_public_id = str(uuid.uuid4())
+    list_users = [user_public_id, str(uuid.uuid4())]
+    for match_public_id in match_public_ids:
+        for user_id in list_users:
+            await async_client.post(
+                f"{test_settings.API_V1_STR}/matches/{match_public_id}/players/",
+                headers=x_api_key_header,
+                json={"user_public_id": user_id},
+            )
+    response = await async_client.get(
+        f"{test_settings.API_V1_STR}/players/{user_public_id}/matches/",
+        headers=x_api_key_header,
+    )
+    assert response.status_code == 200
+    content = response.json()
+    assert content["count"] == n_matches
+    for response_data in content["data"]:
+        assert response_data["public_id"] in match_public_ids
+        assert response_data["court_id"] == 0
+        assert response_data["time"] >= 0 and response_data["time"] < n_matches
+        assert response_data["date"] == "2024-11-25"
+        assert response_data["status"] == ReserveStatus.provisional
+        assert len(response_data["match_players"]) == 2
+        for player_data in response_data["match_players"]:
+            assert player_data["user_public_id"] in list_users
             assert player_data["match_public_id"] in match_public_ids
             assert player_data["reserve"] == ReserveStatus.provisional
