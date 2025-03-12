d336942c3afbc933e88fa62fa1fe7d5ae9ea242b
Belletti Gabriel Ignacio <44848915+GIBelletti@users.noreply.github.com>
2025-03-12 15:47:41 -0300

    refactor: add matches extended.py

--- app/api/routes/players_matches.py
+++ app/api/routes/players_matches.py
@@ -2,7 +2,7 @@ from uuid import UUID
 
 from fastapi import APIRouter, status
 
-from app.models.match import MatchesExtendedListPublic
+from app.models.match_extended import MatchesExtendedListPublic
 from app.services.get_player_matches_service import GetPlayerMatchesService
 from app.services.match_player_service import MatchPlayerService
 from app.utilities.dependencies import SessionDep
--- app/models/match.py
+++ app/models/match.py
@@ -5,8 +5,6 @@ from uuid import UUID, uuid4
 from sqlalchemy import UniqueConstraint
 from sqlmodel import Field, SQLModel
 
-from app.models.match_player import MatchPlayer, MatchPlayerPublic
-
 
 class MatchStatus(str, Enum):
     provisional = "Provisional"
@@ -78,34 +76,3 @@ class MatchFilters(MatchBase, MatchInmutable):
     time: int | None = None
     date: datetime.date | None = None
     status: str | None = None
-
-
-class MatchExtendedPublic(MatchBase, MatchInmutable):
-    match_players: list[MatchPlayerPublic]
-
-    @classmethod
-    def from_private(
-        cls, match: Match, match_players_private: list[MatchPlayer]
-    ) -> "MatchExtendedPublic":
-        data = match.model_dump()
-        data["match_players"] = [
-            MatchPlayerPublic.from_private(x) for x in match_players_private
-        ]
-        return cls(**data)
-
-
-class MatchesExtendedListPublic(SQLModel):
-    data: list[MatchExtendedPublic]
-    count: int
-
-    @classmethod
-    def from_private(
-        cls, all_info: list[tuple[Match, list[MatchPlayer]]]
-    ) -> "MatchesExtendedListPublic":
-        data = []
-        for match, match_players in all_info:
-            match_extend_public = MatchExtendedPublic.from_private(match, match_players)
-            data.append(match_extend_public)
-        data = data
-        count = len(data)
-        return cls(data=data, count=count)
